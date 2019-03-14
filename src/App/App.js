0a61550654f85e7303505c1ec9e0e7ded8c6aa54
Mason France <masonrf1@gmail.com>
2019-03-13 19:41:33 -0600

    refine requests for people

--- src/App/App.js
+++ src/App/App.js
@@ -42,9 +42,32 @@ export default class App extends Component {
       const peopleUrl = 'https://swapi.co/api/people'
       fetch(peopleUrl)
         .then(response => response.json())
-        // .then(people => this.fetchInfo(people))
+        .then(people => this.fetchHomeInfo(people))
+        .then(people => this.fetchSpeciesInfo(people))
+        .then(people => this.setState({people}))
+        .then(() => this.setState({ display: 'people' }))
         .catch(error => this.setState({ error: error.message }))
+    } else {
+      this.setState({ display: 'people' });
     }
+  }
+
+  fetchHomeInfo(people) {
+    const promisesHome = people.results.map(person => {
+      return fetch(person.homeworld)
+        .then(response => response.json())
+        .then(result => ({...person, homeworld: result.name, population: result.population}))
+    })
+    return Promise.all(promisesHome)
+  }
+
+  fetchSpeciesInfo(people) {
+    const promisesSpecies = people.map(person => {
+      return fetch(person.species)
+        .then(response => response.json())
+        .then(result => ({...person, species: result.name}))
+    })
+    return Promise.all(promisesSpecies)
   }
 
   // fetchInfo = (people) => {
@@ -101,6 +124,8 @@ export default class App extends Component {
 
   render() {
     console.log(this.state.people)
+    console.log(this.state.display)
+
     return (
       <div className="App">
         <Header viewFavorites={this.viewFavorites}
